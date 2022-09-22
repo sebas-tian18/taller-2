4fdecd334efb89b1f99a928f21710cd65a7c60d9
Sebastian <luxioxbox49@gmail.com>
2022-09-21 22:17:17 -0300

    manejo de datos

---
+++ test/resources/views/prueba.blade.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <form action="{{ route('mostrar') }}" method ='GET'>
+        @csrf
+            <label for="persona">Persona</label>
+            <select name="persona1" id="persona1">
+                <option value=""></option>
+                <option value='id_persona'>ID</option>
+                <option value='nombre'>Nombre</option>
+                <option value="apellido">Apellido</option>
+                <option value="fecha_nac">Fecha Nacimiento</option>
+                <option value="cedula_ident">Cedula de Identidad</option>
+                <option value="direccion">Direccion</option>
+                <option value="FK_id_sexo">Sexo</option>
+            </select>
+
+            <label for="Persona2">Persona</label>
+            <select name="persona2" id="persona2">
+                <option value=""></option>
+                <option value="id_persona">ID</option>
+                <option value="nombre">Nombre</option>
+                <option value="apellido">Apellido</option>
+                <option value="fecha_nac">Fecha Nacimiento</option>
+                <option value="cedula_ident">Cedula de Identidad</option>
+                <option value="direccion">Direccion</option>
+                <option value="FK_id_sexo">Sexo</option>
+            </select>
+            <input type="text" name="caja">
+            <button>Enviar</button>
+    </form>
+</body>
+</html>
---
+++ test/resources/views/vista.blade.php
@@ -0,0 +1,39 @@
+
+
+<!DOCTYPE html>
+<html lang=”en”>
+<head>
+    <meta charset=”UTF-8″ />
+    <title>test</title>
+</head>
+    <body>
+        <div class = "card-body">
+            <table class = "table">
+                
+                <thead>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Apellido</th>
+                    <th>Fecha Nacimiento</th>
+                    <th>Cedula</th>
+                    <th>Direccion</th>
+                    <th>Sexo</th>
+                </thead>
+                <tbody>
+                @foreach($datos as $per)
+                    <tr>
+                        <td>{{$per->id_persona}}</td>
+                        <td>{{$per->nombre}}</td>
+                        <td>{{$per->apellido}}</td>
+                        <td>{{$per->fecha_nac}}</td>
+                        <td>{{$per->cedula_ident}}</td>
+                        <td>{{$per->direccion}}</td>
+                        <td>{{$per->FK_id_sexo}}</td>
+                        
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </body>
+</html>
