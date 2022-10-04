48d9ae323cc729b029caeb3adc9716a214e880a9
Sebastian <luxioxbox49@gmail.com>
2022-10-04 19:27:58 -0300

    funciones nuevas
    se creo una funcion nueva en personaController
    se cambiaron las rutas
    se cambio la ruta de consultas dinamicas en welcome
    se creo una nueva vista con las consultas dinamicas

--- gen_rep/app/Http/Controllers/personaController.php
+++ gen_rep/app/Http/Controllers/personaController.php
@@ -6,8 +6,6 @@ use Illuminate\Http\Request;
 use App\Models\persona;
 use FFI;
 use Maatwebsite\Excel\Facades\Excel;
-Use Barryvdh\DomPDF\Facade;
-use PDF;
 
 use App\Exports\personaExport;
 
@@ -61,26 +59,15 @@ class personaController extends Controller
     {
         return view('prueba');
     }
-//  FUNCIONES DE EXPORTACION
-////////////////////////////////////////////////////////////////////////////////////////////
 
     public function exportExcel()
     {
-     return Excel::download(new personaExport, 'persona-list.xlsx');
-      /*comando en consola para usar este metodo es: php artisan make:export personaExport --model=persona
-es una libreria para poder usar excel: composer require maatwebsite/excel*/
-    }
-    public function exportPDF(){
-        
-        $pdf = \PDF::loadView('expo_p');
-        return $pdf->stream('expo_p.pdf');//download
-    }
-
-    public function exportjson()
-    {
-        $categorias = persona::all();
-        $data = ['personas'=>$categorias];
-        return response()->json($data,200,[]);
+     return Excel::download(new personaExport, 'persona-list.xlsx'); /*comando en consola para usar este metodo es
+                                                                       php artisan make:export personaExport --model=persona */ 
+                                                                       /*
+                                                                        es una libreria para poder usar excel
+                                                                        composer require maatwebsite/excel
+                                                                        */
     }
 
 }
@@ -89,3 +76,37 @@ es una libreria para poder usar excel: composer require maatwebsite/excel*/
 
 
 
+/*$dato = DB::table('persona')->where('nombre', 'Pedro')->first();
+        dd($dato);*/
+
+/*
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\DB;
+use App\Models\persona;
+use Illuminate\http\Request;
+
+class personaController extends Controller
+{
+    public function index(Request $request){
+        $per = $request->persona1;
+        $per2 = $request->persona2;
+        $caja = $request->caja;
+
+        print_r($caja);
+        var_dump((bool)$per);
+        var_dump((bool)$per2);
+        /*$datos = DB::table('persona')
+            ->when($persona1,function($query,$persona1){
+                $query->select('persona1',$persona1);
+            })
+            ->when($persona2,function($query,$persona2){
+                $query->addSelect('persona2',$persona2);
+            })
+            ->get();
+            dd($datos);
+            //return view('mostrar')->with('datos',$datos);
+    }
+} */
--- gen_rep/config/app.php
+++ gen_rep/config/app.php
@@ -174,7 +174,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Barryvdh\DomPDF\ServiceProvider::class,
         Maatwebsite\Excel\ExcelServiceProvider::class,
 
     ],
@@ -231,7 +230,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'PDF' => Barryvdh\DomPDF\Facade::class,
+
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
 
     ],
